441dc86e350317084685a829a0836f7d9f443fd0
minhhoang11 <dohoangre@gmail.com>
2024-08-31 15:18:50 +0700

    HTBT

---
+++ BaiTap003/BaiTap003/Controllers/NhomControllers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap003.Controllers
+{
+    public class NhomControllers : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
